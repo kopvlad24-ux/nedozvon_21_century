204a2757e1242e6949a774e6c9c17d51de785cac
vladboss0123-pixel <vladboss0123@gmail.com>
2026-05-25 14:20:29 +0300

    Add CORS headers to fix browser fetch from amocrm.ru

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const { google } = require('googleapis');
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 const PORT = process.env.PORT || 3000;
 const AMO_DOMAIN = process.env.AMO_DOMAIN;
